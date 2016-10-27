03fbcfb2ef1b567e02096ff465136c2c82e5c1eb
Tommy Oh <tommyhyungjinoh@gmail.com>
2016-10-26 20:05:55 -0700

    initialized demo-project for SPA.

---
+++ SPA/demo-project/server.js
@@ -0,0 +1,31 @@
+var express 		= require('express'),
+	bodyParser		= require('body-parser'),
+	path			= require('path'),
+	sassMiddleware	= require('node-sass-middleware'),
+	root			= __dirname,
+	port			= process.env.PORT || 8000,
+	app				= express();
+
+app.use(
+	sassMiddleware({
+		src: path.join(root, 'client/static/sass'),
+		dest: path.join(root, 'client/static/css'),
+		prefix: '/static/css',
+		debug: true,
+		outputStyle: 'compressed'
+	})
+
+);
+
+app.use( express.static(path.join(root, 'client')));
+app.use( bodyParser.urlencoded({ extended: true }));
+app.use( bodyParser.json());
+
+require('./server/config/mongoose');
+require('./server/config/routes')(app);
+
+var server = app.listen(port, function() {
+	console.log(`listening to port ${ port }`);
+});
+
+require('./server/config/sockets')(server);
