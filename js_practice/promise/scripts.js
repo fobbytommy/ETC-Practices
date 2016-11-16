ada0bc85b5837507e5e32e2878384c5d8780a946
Tommy Oh <tommyhyungjinoh@gmail.com>
2016-11-15 22:35:37 -0800

    added minor promise practice.

---
+++ js_practice/promise/scripts.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var arr = [{
+  "id": 1,
+  "first_name": "Donald",
+  "last_name": "Washington",
+}, {
+  "id": 2,
+  "first_name": "Brenda",
+  "last_name": "Gonzales",
+}, {
+  "id": 3,
+  "first_name": "Rachel",
+  "last_name": "Edwards",
+}];
